93c0c9975552d444f1e3f0a59f6d6cb4f70dcfd9
Assem.Barakat <Assem.Barakat@GGSTEC10999LAP>
2025-06-29 14:16:15 +0300

    Add project files.

---
+++ Automation KT Assignments/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Automation_KT_Assignments
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
